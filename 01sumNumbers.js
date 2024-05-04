ec534038eb900ea80415c2b41a66999a77bbb654
Jane <tanhuiqing95@hotmail.com>
2024-05-05 00:04:44 +0800

    sumNumbers

---
+++ 01sumNumbers.js
@@ -0,0 +1,5 @@
+// sumNumbers
+// Prompt:
+
+// Write a function called sumNumbers that accepts a single array of numbers and returns the sum of the numbers in the array.
+// If the array is empty, return 0 (zero).
